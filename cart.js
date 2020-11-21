8b15dabe38e3f29040756d25f87fa05b4b292539
Sarah <sara-alshareef2011@hotmail.com>
2020-11-21 13:00:53 +0200

    moved the style from the script.js file#21

--- cart.js
+++ cart.js
@@ -22,7 +22,8 @@ let cards = document.createElement("div");
 let cardsContainer = document.getElementsByClassName("cards-container")[0];
 cardsContainer.appendChild(cards);
 
-products.map(function(product){
+products.forEach(function(product){
+    
     let card= document.createElement("div");
     let cardImage= document.createElement("img");    
     let firstRowContainer = document.createElement("div");
@@ -37,11 +38,11 @@ products.map(function(product){
     let totalPrice= document.getElementsByClassName("total-price")[0];
 
     cardImage.src=product.imageURL;
-    cardName.innerHTML=product.name;
-    cardPrice.innerHTML=product.price;
-    cardCategory.innerHTML=product.category;
+    cardName.textContent=product.name;
+    cardPrice.textContent=product.price;
+    cardCategory.textContent=product.category;
     deleteBtn.innerHTML='<i class="fas fa-trash-alt"></i>';
-    quantityText.innerHTML="Quantity:";
+    quantityText.textContent="Quantity:";
     quantity.type="number";
     quantity.min="1";
     quantity.placeholder="1";
@@ -60,47 +61,13 @@ products.map(function(product){
     let cardContainer = document.getElementsByClassName("card-container")[0];
     cardContainer.appendChild(card);
     
-    card.style.display="flex";
-    card.style.flexDirection="column";
-    card.style.border="solid 1px #3C9B8A";
-    card.style.padding="25px";
-    card.style.margin= "2%";
-    card.style.boxShadow="-5px 6px 16px -6px rgba(0,0,0,0.59)";
-
-    cardImage.style.width="225px";
-    cardImage.style.height="225px";
-    cardImage.style.marginBottom="10px";
-    cardName.style.fontSize="23px";
-    cardImage.style.marginLeft="8.5px";
-
-    firstRowContainer.style.display="flex";
-    firstRowContainer.style.justifyContent="space-between";
-    firstRowContainer.style.alignItems="center";
-    firstRowContainer.style.padding="5px";
-    firstRowContainer.style.fontSize="20px";
-    firstRowContainer.style.fontWeight="700";
-
-    secondRowContainer.style.display="flex";
-    secondRowContainer.style.justifyContent="space-between";
-    secondRowContainer.style.alignItems="center";
-    secondRowContainer.style.fontSize="20px";
-    secondRowContainer.style.padding="5px";
-
-    deleteBtn.style.border="none";
-    deleteBtn.style.background="white";
-    deleteBtn.style.fontSize="30px";
-    deleteBtn.style.cursor="pointer";
-
-    quantityContainer.style.display="flex";
-    quantityContainer.style.justifyContent="center";
-    quantityContainer.style.alignItems="center";
-    quantityContainer.style.fontSize="15px";
-    quantityContainer.style.fontWeight="600";
-
-    quantity.style.border="none";
-    quantity.style.width="10%";
-    quantity.style.outline="0";
-    quantity.style.marginLeft="10px";
+    card.className="card";
+    cardImage.className="card-image";
+    firstRowContainer.className="first-row-container";
+    secondRowContainer.className="second-row-container";
+    deleteBtn.className="delete-btn";
+    quantityContainer.className="quantity-container";
+    quantity.className="quantity";
 
     deleteBtn.addEventListener('click',function(event){
         var buttonClicked= event.target;
@@ -110,10 +77,7 @@ products.map(function(product){
     
     cards.appendChild(card);
 })
-cards.style.padding="2%";
-cards.style.display='flex';
-cards.style.flexWrap='wrap';
-cards.style.width="100%";
+cards.className="cards";
 // updateTotalPrice(){
 
 // };
