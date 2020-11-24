d2256a3b47c865b6056a79ce19211fdf194f077e
unknown <obaidofj@gmail.com>
2020-11-24 08:39:55 -0800

    javascript done for add and edit forms

--- add-edit.js
+++ add-edit.js
@@ -4,10 +4,10 @@ let products = read("products");
 let idKey = 0;
 
 if (!products.length) {
-  idKey = 1;
+    idKey = 1;
 } else {
-  let lastItem = products[products.length - 1];
-  idKey = Number(lastItem.id) + 1;
+    let lastItem = products[products.length - 1];
+    idKey = Number(lastItem.id) + 1;
 }
 let name = document.getElementById("name");
 let desc = document.getElementById("desc");
@@ -17,70 +17,70 @@ let imagUrl = document.getElementById("imagUrl");
 let add_prod_form = document.getElementById("add-prod-form");
 let idValue = document.getElementById("id");
 
-add_but.addEventListener("click", function (e) {
-  e.preventDefault();
-  if (e.target.value == "Add") {
-    products.push({
-      id: idKey,
-      name: name.value,
-      desc: desc.value,
-      price: price.value,
-      category: category.value,
-      imageURL: imagUrl.value,
-    });
+add_but.addEventListener("click", function(e) {
+    e.preventDefault();
+    if (e.target.value == "Add") {
+        products.push({
+            id: idKey,
+            name: name.value,
+            desc: desc.value,
+            price: price.value,
+            category: category.value,
+            imageURL: imagUrl.value,
+        });
+        name.value = "";
+        desc.value = "";
+        price.value = "";
+        category.value = "";
+        imagUrl.value = "";
+
+        save(products);
+    } else if (e.target.value == "Save") {
+        let ind = products.findIndex(findItem, idValue.value);
+        products[ind] = {
+            id: idValue.value,
+            name: name.value,
+            desc: desc.value,
+            price: price.value,
+            category: category.value,
+            imageURL: imagUrl.value,
+        };
+        save(products);
+    }
+});
+
+var modal = document.getElementById("addEditProductModal");
+
+var btn = document.getElementById("addProductBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function(e) {
+    let formTitle = document.getElementById("formTitle");
+    let editBtn = document.getElementById("add-button");
+    let cancelBtn = document.getElementById("cancelBtn");
+
+    formTitle.innerText = "Add Product";
+    editBtn.value = "Add";
+    cancelBtn.style.display = "none";
+
     name.value = "";
     desc.value = "";
     price.value = "";
     category.value = "";
     imagUrl.value = "";
 
-    save(products);
-  } else if (e.target.value == "Save") {
-    let ind = products.findIndex(findItem, idValue.value);
-    products[ind] = {
-      id: idValue.value,
-      name: name.value,
-      desc: desc.value,
-      price: price.value,
-      category: category.value,
-      imageURL: imagUrl.value,
-    };
-    save(products);
-  }
-});
-
-var modal = document.getElementById("addProductModal");
-
-var btn = document.getElementById("addProductBtn");
-
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function (e) {
-  let formTitle = document.getElementById("formTitle");
-  let editBtn = document.getElementById("add-button");
-  let cancelBtn = document.getElementById("cancelBtn");
-
-  formTitle.innerText = "Add Product";
-  editBtn.value = "Add";
-  cancelBtn.style.display = "none";
-
-  name.value = "";
-  desc.value = "";
-  price.value = "";
-  category.value = "";
-  imagUrl.value = "";
-
-  modal.style.display = "block";
+    modal.style.display = "block";
 };
 
-span.onclick = function () {
-  modal.style.display = "none";
+span.onclick = function() {
+    modal.style.display = "none";
 };
 
-window.onclick = function (event) {
-  if (event.target == modal) {
-    //modal.style.display = "none";
-  }
+window.onclick = function(event) {
+    if (event.target == modal) {
+        //modal.style.display = "none";
+    }
 };
 
 let editBtn = document.getElementById("editProductBtn");
@@ -90,26 +90,26 @@ let editBtn = document.getElementById("editProductBtn");
 //};
 
 function findItem(v) {
-  return v.id == this;
+    return v.id == this;
 }
 
 function editProd(itemid) {
-  products = read("products");
-  console.log(products);
-  let formTitle = document.getElementById("formTitle");
-  let editBtn = document.getElementById("add-button");
-  let cancelBtn = document.getElementById("cancelBtn");
+    products = read("products");
+    console.log(products);
+    let formTitle = document.getElementById("formTitle");
+    let editBtn = document.getElementById("add-button");
+    let cancelBtn = document.getElementById("cancelBtn");
 
-  formTitle.innerText = "Edit Product";
-  editBtn.value = "Save";
-  cancelBtn.style.display = "block";
+    formTitle.innerText = "Edit Product";
+    editBtn.value = "Save";
+    cancelBtn.style.display = "block";
 
-  modal.style.display = "block";
-  iteminfo = products.find(findItem, itemid);
-  idValue.value = itemid;
-  name.value = iteminfo.name;
-  desc.value = iteminfo.desc;
-  price.value = iteminfo.price;
-  category.value = iteminfo.category;
-  imagUrl.value = iteminfo.imageURL;
+    modal.style.display = "block";
+    iteminfo = products.find(findItem, itemid);
+    idValue.value = itemid;
+    name.value = iteminfo.name;
+    desc.value = iteminfo.desc;
+    price.value = iteminfo.price;
+    category.value = iteminfo.category;
+    imagUrl.value = iteminfo.imageURL;
 }
---
+++ search-filter.js
@@ -0,0 +1,60 @@
+let products = read("products");
+
+let search = document.getElementById("search");
+let searchBtn = document.getElementById("searchBtn");
+let categoryList = document.getElementById("category");
+let priceFrom = document.getElementById("priceFrom");
+let priceTo = document.getElementById("priceTo");
+
+search.addEventListener("keypress", function(e) {
+    if (e.key === "Enter") {
+        searchName(e.target.value);
+    }
+});
+
+searchBtn.addEventListener("click", function() {
+    searchName(search.value);
+});
+
+categoryList.addEventListener("change", function(e) {
+    let result = products.filter(function(item) {
+        return item.category == e.target.value;
+    });
+    showItems(result);
+});
+
+priceFrom.addEventListener("input", function(e) {
+    let result = products.filter(function(item) {
+
+        if (!priceTo.value) {
+            return Number(item.price) >= Number(e.target.value);
+        } else {
+            return Number(item.price) >= Number(e.target.value) && Number(item.price) <= Number(priceTo.value)
+        }
+    });
+    showItems(result);
+});
+
+priceTo.addEventListener("input", function(e) {
+    let result = products.filter(function(item) {
+        if (!priceFrom.value) {
+            return Number(item.price) <= Number(e.target.value);
+        } else {
+            return Number(item.price) >= Number(priceFrom.value) && Number(item.price) <= Number(e.target.value)
+        }
+
+
+    });
+    showItems(result);
+});
+
+function searchName(text) {
+    let result = products.filter(function(item) {
+        return item.name.toUpperCase().includes(text.toUpperCase());
+    });
+    showItems(result);
+}
+
+function showItems(res) {
+    console.log(res);
+}
